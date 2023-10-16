baec18d50fad99e3145c6a0c9f6da2780018bb69
SmortBoy65 <92156281+SmortBoy65@users.noreply.github.com>
2023-10-16 14:01:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -81,8 +81,8 @@ async def pm_text(bot, message):
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
     await message.reply_text(
-         text=f"<b>ʜᴇʏ {user} 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ɪᴛ ɪɴ ᴏᴜʀ <a href=https://t.me/vj_bots>ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a> ᴏʀ ᴄʟɪᴄᴋ ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ 👇</b>",   
-         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ", url=f"https://t.me/vj_bots")]])
+         text=f"<b>Hᴇʏ {user} ⚡️ ,\n\n<Sᴇɴᴅ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ/sᴇʀɪᴇs/b>",   
+         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝Rᴇᴏ̨ᴜᴇsᴛ ɢʀᴏᴜᴘ", url=f"https://t.me/typeanymovieHD4")]])
     )
     await bot.send_message(
         chat_id=LOG_CHANNEL,
