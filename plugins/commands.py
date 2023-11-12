7b5bff322e0dd1bfbb88e175af380257f22eb6ed
ZeinToji <148427450+ZeinToji@users.noreply.github.com>
2023-11-12 11:03:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -161,7 +161,7 @@ async def start(client, message):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],
-                            [
+                           # [
                                 InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
                             ]
                         ]
